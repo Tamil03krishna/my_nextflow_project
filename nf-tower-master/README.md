b86fd7c649d6be57b4e31ab37aa0b287a58fe22d
tamilselvan <gokul@latlontechnologies.com>
2025-06-13 15:39:43 +0530

    sample commit

--- nf-tower-master/README.md
+++
@@ -1,3 +0,0 @@
-## nf-tower 
-
-This project has been archived and it's not maintained anymore. Consider using https://cloud.seqera.io/
--- nf-tower-master/tower-web/src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: "/api",
-  version: "19.08.0",
-  commitId: "63a81b5.dirty",
-  captchaKey: null,
-};
